26f8c159a1489cb16d30a7e8192adde1d0cd5b5d
Daniel <daniel13081@outlook.com>
2017-03-27 10:33:37 -0500

    agregados onblur

--- registro.php
+++ registro.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="public/css/registro.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="public/js/registro.js"></script>
     <script>
@@ -76,7 +77,7 @@ Diligencie el formulario y asi posteriormente ingresar a la p&aacute;gina.</p>
         <div class="col-md-6">
             <div class="form-group">
                 <label for="exampleInputEmail1">Correo</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Correo" onblur="revisar(this) revisaremail(this)">
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Correo" onblur="revisar(this); revisaremail(this);">
             </div>
         </div>
     </div>
