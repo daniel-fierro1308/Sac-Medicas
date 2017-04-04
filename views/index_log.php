49ab7e33514618657c99a8be72864bd703ac75c9
Daniel <daniel13081@outlook.com>
2017-04-04 17:57:03 -0500

    add logueado.php and fixed index_log.php

--- views/index_log.php
+++ views/index_log.php
@@ -1,11 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
 <?php
- 
+ include('logueado.php');
+ include('./controladores/conexion.php');
 session_start();
 if(!isset($_SESSION["documento"])){
   header("location:login.php");
+} else {
+$usuario = $_SESSION['documento']; 
+$consulta = "SELECT nombre,apellido FROM usuarios WHERE documento = '$usuario'";
+$resultado = mysqli_query($con, $consulta);
+$row= mysqli_fetch_array($resultado);
 }
- 
-echo '<h1 align=center>Bienvenido usuario:'.$_SESSION["documento"].'</h1>';
-echo '<p align=center><a href="./controladores/logout.php">Logout</a></p>';
- 
 ?>
+   
+<strong><p id="user"> <?php echo $row['nombre']; ?>   <?php echo $row['apellido']; ?>  </p></strong>
+</body>
+</html> 
