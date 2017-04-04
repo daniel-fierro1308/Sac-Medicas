c917525157177ae1ad680fb5795e7b5ecc824b43
Daniel <daniel13081@outlook.com>
2017-04-04 16:13:00 -0500

    add footer.css and fixed changes

--- views/contacto.php
+++ views/contacto.php
@@ -8,5 +8,58 @@
     <?php 
         require('layout.php');
     ?>
+
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-3" align="center">Formulario de Contacto</h1>
+            <p class="lead" align="center">Mediante este formulario podras contactarte con nosotros, envianos tus inquietudes, sugerencias o demas.</p>
+        </div> 
+        <form>
+            <div class="formulario">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Nombre(requerido):</label>
+                            <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Nombre:(s)">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>E-mail:</label>
+                            <input type="text" id="nombre" name="nombre" class="form-control" placeholder="E-mail">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Teléfono:</label>
+                            <input type="number" id="nombre" name="nombre" class="form-control" placeholder="Telefono">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Asunto:</label>
+                            <input type="text" id="nombre" name="nombre" class="form-control" placeholder="Asunto">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label for="exampleTextarea">Mensaje:</label>
+                            <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="button" class="btn btn-success" role="button" id="enviar" name="enviar">Enviar Mensaje</button>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
--- views/footer.php
+++ views/footer.php
@@ -2,10 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../public/css/footer.css">
 </head>
 <body>
-    <footer>
-        
-    </footer>
+    <div id="footer" align="center" style="margin-top:5%">
+        <footer>
+            <strong>
+            <p style="margin-top:10px">CENTRO DE SALUD RAFAEL REYES - Dirección: CALLE 9 No 5A-33 APULO</p>
+            <p>Teléfono: (01) 57 8397073</p>
+            <p style="margin-bottom:10px">Ciudad: Apulo - Cundinamarca</p>
+            </strong>
+        </footer>
+    </div>
+
 </body>
 </html>
