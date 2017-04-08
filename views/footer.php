1f8e8cf208a0ec9e401af953b93260f948dd3cd1
Daniel <daniel13081@outlook.com>
2017-04-09 00:53:23 +0200

    footer

--- views/footer.php
+++ views/footer.php
@@ -3,15 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../public/css/footer.css">
+    <link rel="stylesheet" href="../public/img/footer/style.css">
 </head>
 <body>
-    <div id="footer" align="center" style="margin-top:5%">
+    <div class="footer" align="center" style="margin-top:5%">
         <footer>
-            <strong>
-            <p style="margin-top:10px">CENTRO DE SALUD RAFAEL REYES - Dirección: CALLE 9 No 5A-33 APULO</p>
-            <p>Teléfono: (01) 57 8397073</p>
-            <p style="margin-bottom:10px">Ciudad: Apulo - Cundinamarca</p>
-            </strong>
+        <h2>Contactate por Redes sociales</h2>
+           <ul>
+               <li><a href="https://www.facebook.com/daniel.fierro.torres"><span class="icon-facebook2"></span></a></li>
+               <li></li>
+               <li></li>
+               <li></li>
+           </ul>
         </footer>
     </div>
 
