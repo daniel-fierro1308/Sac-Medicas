7472db3a2ac94d98846c4624c7dc56673232eb08
Daniel <daniel13081@outlook.com>
2017-04-17 17:10:26 -0500

    fixed modificar_info

--- views/controladores/modificar_info.php
+++ views/controladores/modificar_info.php
@@ -7,17 +7,28 @@
     $user = $_SESSION["usuario"];
     $alerta = "";
 
-    $sql = "SELECT correo FROM usuarios WHERE correo= '$correo'";
+    $sql = "SELECT correo FROM usuarios WHERE correo = '$correo' AND usuario = '$user'";
     $res = mysqli_query($con, $sql);
-    $data = mysqli_fetch_array($res);
-        
+    $datas = mysqli_fetch_array($res);
+    $email = $datas["correo"];       
 
+     $sql = "SELECT correo FROM usuarios WHERE correo = '$correo'";
+     $result = mysqli_query($con, $sql);
+     $datos = mysqli_fetch_array($result);
 
-    if($alerta != ""){
-        echo "<div class='alert alert-dismissible alert-danger'><strong>Error </strong>$alerta</div>";
-    } else {
-         $sql = "UPDATE usuarios SET telefono = '$telefono', correo = '$correo' WHERE usuario = '$user'";
-         mysqli_query($con, $sql);
-         echo "<div class='alert alert-dismissible alert-success'><strong>¡Correcto!</strong><br>Su informacion se ha actualizado correctamente</div>";
-    }
+        if($datos != $datas) {
+            echo "<div class='alert alert-dismissible alert-danger'><strong>Error </strong>El correo ingresado ya esta registrado en otra cuenta</div>";
+        } else if($correo == $email && $datos == $datas || $correo != $email) {
+            $sql = "UPDATE usuarios SET telefono = '$telefono', correo = '$correo' WHERE usuario = '$user'";
+            mysqli_query($con, $sql);
+            echo "<div class='alert alert-dismissible alert-success'><strong>¡Correcto!</strong><br>Su informacion se ha actualizado correctamente</div>";
+        }
+
+    // if($alerta != ""){
+    //     echo "<div class='alert alert-dismissible alert-danger'><strong>Error </strong>$alerta</div>";
+    // } else {
+    //      $sql = "UPDATE usuarios SET telefono = '$telefono', correo = '$correo' WHERE usuario = '$user'";
+    //      mysqli_query($con, $sql);
+    //      echo "<div class='alert alert-dismissible alert-success'><strong>¡Correcto!</strong><br>Su informacion se ha actualizado correctamente</div>";
+    // }
 ?>
